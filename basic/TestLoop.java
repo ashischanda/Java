ec4780cc9280a7eb60237b8d5ccbcd220bfa8b90
Ashis Kumar Chanda <ashis415@yahoo.com>
2015-02-20 22:54:19 +0600

    Update and rename basic to basic/TestLoop.java

---
+++ basic/TestLoop.java
@@ -0,0 +1,29 @@
+/**
+ *
+ * @author Ashis Kumar Chanda
+ */
+public class TestLoop {
+
+    public static void main(String args[]){
+        System.out.println("ashis");
+        int i;
+        // Loop type 1
+        for(i=0;i <2; i++){
+            System.out.println("go");
+        }
+
+        // Loop type 2
+        while(i<5){
+            System.out.println("love");
+        i++;
+        }
+        
+        // Loop type 3
+        do{
+            System.out.println("lo");
+            i++;
+        }while(i<8);
+
+    }
+
+}
