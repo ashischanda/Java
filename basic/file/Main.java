62878c0f876e00d9c5ca460deca94f95ffe718bb
Ashis Kumar Chanda <ashis415@yahoo.com>
2015-04-07 20:52:48 +0600

    Create Main.java

---
+++ basic/file/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Give a num: ");
+        int arr[] = new int[5];
+        try{
+          
+        }
+       
+        catch(Exception e){
+            System.out.println("Error: "+e);
+        }
+        finally{
+          
+        }
+
+      
+
+    }
+
+}
